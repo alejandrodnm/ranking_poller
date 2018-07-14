7e028b6f0965b382efac222394c61c614709bb2d
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2018-07-14 10:15:06 +0200

    Update mix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule StandingsPoller.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -15,6 +16,13 @@ defmodule StandingsPoller.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      # Docs dependencies
+      {:ex_doc, "~> 0.18.0", only: :docs, runtime: false},
+      {:inch_ex, "~> 0.5", only: [:docs]},
+      {:credo, "~> 0.9", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.8", only: :test}
+    ]
   end
 end
