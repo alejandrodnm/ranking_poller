9f859405229af3ca674961cd6142f62c9f2bf9c7
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2018-08-29 21:14:29 +0200

    Fix Ranking.Import

--- apps/ranking/lib/ranking/import.ex
+++ apps/ranking/lib/ranking/import.ex
@@ -10,6 +10,8 @@ defmodule Ranking.Import do
   import Ecto.Changeset
   use Ecto.Schema
 
+  @typep quote_extra_data :: %{required(String.t()) => pos_integer}
+
   schema "ranking_import" do
     timestamps(type: :utc_datetime, updated_at: false)
     field(:timestamp, :integer)
@@ -42,47 +44,52 @@ defmodule Ranking.Import do
   def run do
     {:ok, ranking} = Provider.get_ranking()
 
-    multi =
-      process_coin(
+    Multi.new()
+    |> Multi.insert(:ranking_import, changeset(%Ranking.Import{}, ranking["metadata"]))
+    |> Multi.merge(fn %{ranking_import: ranking_import} ->
+      Multi.new()
+      |> process_coin(
         Map.to_list(ranking["data"]),
-        ranking["metadata"]["timestamp"],
-        Multi.new()
+        %{
+          "timestamp" => ranking["metadata"]["timestamp"],
+          "ranking_import_id" => ranking_import.id
+        }
       )
-
-    Repo.transaction(multi)
+    end)
+    |> Repo.transaction()
   end
 
-  @spec process_coin(list(map()), pos_integer(), Multi.t()) :: Multi.t()
-  defp process_coin([], _, multi) do
+  @spec process_coin(Multi.t(), list(map()), quote_extra_data) :: Multi.t()
+  defp process_coin(multi, [], _) do
     multi
   end
 
-  defp process_coin([{id, coin_payload} | tail], timestamp, multi) do
-    quote_payload = get_quote_payload(coin_payload, timestamp)
+  defp process_coin(multi, [{id, coin_payload} | tail], quote_extra_data) do
+    quote_payload = get_quote_payload(coin_payload, quote_extra_data)
 
-    new_multi =
-      multi
-      |> Multi.insert(
-        "coin_#{id}",
-        Coin.changeset(%Coin{}, coin_payload)
-      )
-      |> Multi.insert("quote_#{id}", Quote.changeset(%Quote{}, quote_payload))
-
-    process_coin(tail, timestamp, new_multi)
+    multi
+    |> Multi.insert(
+      "coin_#{id}",
+      Coin.changeset(%Coin{}, coin_payload),
+      on_conflict: :nothing
+    )
+    |> Multi.insert("quote_#{id}", Quote.changeset(%Quote{}, quote_payload))
+    |> process_coin(tail, quote_extra_data)
   end
 
-  @spec get_quote_payload(map(), pos_integer()) :: map()
-  defp get_quote_payload(coin_payload, timestamp) do
+  @spec get_quote_payload(map(), quote_extra_data) :: map()
+  defp get_quote_payload(coin_payload, quote_extra_data) do
     {:ok, quotes} = Map.fetch(coin_payload, "quotes")
     {:ok, usd} = Map.fetch(quotes, "USD")
 
-    Map.merge(
-      usd,
-      %{
-        "coin_id" => coin_payload["id"],
-        "last_updated" => coin_payload["last_updated"],
-        "timestamp" => timestamp
-      }
-    )
+    quote_extra_data
+    |> Map.merge(usd)
+    |> Map.merge(%{
+      "coin_id" => coin_payload["id"],
+      "last_updated" => coin_payload["last_updated"],
+      "circulating_supply" => coin_payload["circulating_supply"],
+      "total_supply" => coin_payload["total_supply"],
+      "max_supply" => coin_payload["max_supply"]
+    })
   end
 end
--- apps/ranking/test/import_test.exs
+++ apps/ranking/test/import_test.exs
@@ -1,6 +1,9 @@
 defmodule Ranking.ImportTest do
+  alias Persistence.Repo
+  alias Ranking.Coin
   alias Ranking.Import
   alias Ranking.Test.Factory
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
   use Persistence.DataCase
 
   test "insert a new ranking import" do
@@ -9,5 +12,14 @@ defmodule Ranking.ImportTest do
     assert ranking_import.timestamp == payload["timestamp"]
     assert ranking_import.error == nil
     assert ranking_import.num_cryptocurrencies == payload["num_cryptocurrencies"]
+  end
+
+  test "get ranking" do
+    use_cassette "ranking_get_ranking" do
+      {:ok, ranking} = Ranking.fetch_current()
+    end
+
+    query = from(coin in Coin)
+    coins = Repo.all(Coin)
   end
 end
