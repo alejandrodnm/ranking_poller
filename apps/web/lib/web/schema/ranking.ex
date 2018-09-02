928bd2b402db9149229059021a3667120c751d4a
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2018-09-02 12:06:45 +0200

    Add all the quote fields to the schema

--- apps/web/lib/web/schema/ranking.ex
+++ apps/web/lib/web/schema/ranking.ex
@@ -15,8 +15,17 @@ defmodule Web.Schema.Ranking do
 
   object :quote do
     field(:id, :id)
+    field(:inserted_at, :datetime)
+    field(:price, :decimal)
+    field(:volume_24h, :decimal)
+    field(:market_cap, :decimal)
+    field(:percent_change_1h, :decimal)
+    field(:percent_change_24h, :decimal)
     field(:percent_change_7d, :decimal)
-    # TODO more fields
+    field(:last_updated, :integer)
+    field(:circulating_supply, :decimal)
+    field(:total_supply, :decimal)
+    field(:max_supply, :decimal)
 
     field :coin, :coin do
       resolve(&Ranking.get_coin/3)
